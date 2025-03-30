d7d0ebb9b13d5d29e11b40d062fda20c7a8a8852
micael305 <miicael305@gmail.com>
2025-03-30 18:21:28 -0300

    Consumiendo mi primera API

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 const URL = "https://api.thecatapi.com/v1/images/search";
 
 const cat = document.getElementById('cat');
+const btn = document.getElementById('btn-gen-cat');
 
 async function fetchData(urlAPI){
     const response = await fetch(urlAPI);
-    const data = response.json();
+    const data = await response.json();
     return data;
 }
 
-(async () => {
-    try{
+async function reload() {
     const img = await fetchData(URL);
     cat.src = img[0].url;
-    }catch(error){
-        console.error(error);
-    }
-})();
+}
+
+reload();
+
+//btn.onclick = reload; no hace falta esta en el html del button
+
+
+
+
+
+

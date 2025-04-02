e2b2b9600e5eeb80035fc3fc73b301a08049d1e7
micael305 <miicael305@gmail.com>
2025-04-02 17:50:04 -0300

    eliminar michis y correccion de bugs

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const API_URL_RANDOM = 
-'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_Kuq30cHWHlp1gWjzq56GvZpaN3LFL5axykpIZB5HavHTJbjoAG6S87hqPjo9ePK0';
-
-const API_URL_FAVOURITES = 
-'https://api.thecatapi.com/v1/favourites?api_key=live_Kuq30cHWHlp1gWjzq56GvZpaN3LFL5axykpIZB5HavHTJbjoAG6S87hqPjo9ePK0';
+const api_key = 'live_Kuq30cHWHlp1gWjzq56GvZpaN3LFL5axykpIZB5HavHTJbjoAG6S87hqPjo9ePK0';
+const API_URL_RANDOM = `https://api.thecatapi.com/v1/images/search?limit=2&api_key=${api_key}`;
+const API_URL_FAVOURITES = `https://api.thecatapi.com/v1/favourites`;
+const API_URL_FAVOURITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/api_key=${id}?${api_key}`;
 
 const spanError = document.getElementById('error');
 
 async function loadRandomMichis() {
     const res = await fetch(API_URL_RANDOM);
+    const data = await res.json(); 
     if(res.status !== 200){
         spanError.innerText = `Error: ${res.status}`
      }else{
-        const data = await res.json(); 
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
         const btnRan1 = document.getElementById('btnRan1');
@@ -22,33 +21,44 @@ async function loadRandomMichis() {
 
         btnRan1.onclick = () => saveFavouriteMichi(data[0].id);
         btnRan2.onclick = () => saveFavouriteMichi(data[1].id);  
-
-     //hacemos el llamado a la funcion de otra funcion
-     // ya que si no se llama aunque no le demos click
+        console.log(data);
      }
     
 }
 
 async function loadFavouritesMichis() {
-    const res = await fetch(API_URL_FAVOURITES);
+    const res = await fetch(API_URL_FAVOURITES, {
+        method: 'GET',
+        headers:{
+                'X-API-KEY': 'live_Kuq30cHWHlp1gWjzq56GvZpaN3LFL5axykpIZB5HavHTJbjoAG6S87hqPjo9ePK0'
+            }
+    });
     const data = await res.json();
-    console.log('load favourite michis');
     if(res.status !== 200){
         spanError.innerText = 'Error: ' + res.status;
      }else{
-        data.forEach(michi => {
-            const section = document.getElementById('favoritesMichis');
+        const section = document.getElementById('favoritesMichis');
+        section.innerHTML = "";
+        const h2 = document.createElement('h2');
+        const h2Text = document.createTextNode('Michis favoritos');
+        h2.appendChild(h2Text);
+        section.appendChild(h2);
+
+        data.forEach(michi => { 
             const article = document.createElement('article');
             const img = document.createElement('img');
             const button = document.createElement('button');
             const buttonTxt = document.createTextNode('Quitar michi de favoritos');
-
+            
             section.appendChild(article);
             article.appendChild(img);
             article.appendChild(button);
             img.src = michi.image.url;
             img.width = 150;
             button.appendChild(buttonTxt);
+
+            //para eliminar michi
+            button.onclick = () => deleteFavouriteMichi(michi.id);
         });
      }
 
@@ -59,8 +69,7 @@ async function  saveFavouriteMichi(id) {
     const res = await fetch(API_URL_FAVOURITES, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json' //ya que podria ser un png por ejemplo y no json
-            //indicamos que json sera el lenguaje comun entre front y back
+            'Content-Type': 'application/json' 
         },
         body: JSON.stringify ({ 
             image_id: id 
@@ -68,21 +77,26 @@ async function  saveFavouriteMichi(id) {
     })
     if(res.status !== 200){
         spanError.innerText = 'Error: ' + res.status;
+     }else{
+        console.log('Michi guardado de favoritos');
+        loadFavouritesMichis();
+     } 
+}
+
+async function deleteFavouriteMichi(id) {
+    const res = await fetch(API_URL_FAVOURITES_DELETE(id), {
+        method: 'DELETE'
+    })
+    if(res.status !== 200){
+        spanError.innerText = 'Error: ' + res.status;
+     }else{
+        loadFavouritesMichis();
+        console.log('Michi eliminado de favoritos');
      }
-     loadFavouritesMichis();
 }
 
 loadRandomMichis();
 loadFavouritesMichis();
-
-//En el body debemos enviarselo no como un objeto de JS si no como string.
-//ya que no sabemos si el lenguaje de nuestra api tambien es JS,
-//puede estar escrito en go, python, etc
-//JSON.stringify(), te aseguras de que los datos que envías al servidor estén
-//en el formato correcto, lo que permite al servidor analizarlos y procesarlos correctamente.
-
-
-
 
 
 

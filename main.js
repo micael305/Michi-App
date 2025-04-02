9069427aa7123e6f3539e207b507c6b72b9e64fc
micael305 <miicael305@gmail.com>
2025-04-02 14:59:12 -0300

    obtener y agregar michis usando api-key

--- main.js
+++ main.js
@@ -1,22 +1,85 @@
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL_RANDOM = 
+'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_Kuq30cHWHlp1gWjzq56GvZpaN3LFL5axykpIZB5HavHTJbjoAG6S87hqPjo9ePK0';
 
-const cat = document.getElementById('cat');
-const btn = document.getElementById('btn-gen-cat');
+const API_URL_FAVOURITES = 
+'https://api.thecatapi.com/v1/favourites?api_key=live_Kuq30cHWHlp1gWjzq56GvZpaN3LFL5axykpIZB5HavHTJbjoAG6S87hqPjo9ePK0';
 
-async function fetchData(urlAPI){
-    const response = await fetch(urlAPI);
-    const data = await response.json();
-    return data;
+const spanError = document.getElementById('error');
+
+async function loadRandomMichis() {
+    const res = await fetch(API_URL_RANDOM);
+    if(res.status !== 200){
+        spanError.innerText = `Error: ${res.status}`
+     }else{
+        const data = await res.json(); 
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        const btnRan1 = document.getElementById('btnRan1');
+        const btnRan2 = document.getElementById('btnRan2');
+
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+
+        btnRan1.onclick = () => saveFavouriteMichi(data[0].id);
+        btnRan2.onclick = () => saveFavouriteMichi(data[1].id);  
+
+     //hacemos el llamado a la funcion de otra funcion
+     // ya que si no se llama aunque no le demos click
+     }
+    
 }
 
-async function reload() {
-    const img = await fetchData(URL);
-    cat.src = img[0].url;
+async function loadFavouritesMichis() {
+    const res = await fetch(API_URL_FAVOURITES);
+    const data = await res.json();
+    console.log('load favourite michis');
+    if(res.status !== 200){
+        spanError.innerText = 'Error: ' + res.status;
+     }else{
+        data.forEach(michi => {
+            const section = document.getElementById('favoritesMichis');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const button = document.createElement('button');
+            const buttonTxt = document.createTextNode('Quitar michi de favoritos');
+
+            section.appendChild(article);
+            article.appendChild(img);
+            article.appendChild(button);
+            img.src = michi.image.url;
+            img.width = 150;
+            button.appendChild(buttonTxt);
+        });
+     }
+
+     console.log(data);
 }
 
-reload();
+async function  saveFavouriteMichi(id) {
+    const res = await fetch(API_URL_FAVOURITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' //ya que podria ser un png por ejemplo y no json
+            //indicamos que json sera el lenguaje comun entre front y back
+        },
+        body: JSON.stringify ({ 
+            image_id: id 
+        })
+    })
+    if(res.status !== 200){
+        spanError.innerText = 'Error: ' + res.status;
+     }
+     loadFavouritesMichis();
+}
 
-//btn.onclick = reload; no hace falta esta en el html del button
+loadRandomMichis();
+loadFavouritesMichis();
+
+//En el body debemos enviarselo no como un objeto de JS si no como string.
+//ya que no sabemos si el lenguaje de nuestra api tambien es JS,
+//puede estar escrito en go, python, etc
+//JSON.stringify(), te aseguras de que los datos que envías al servidor estén
+//en el formato correcto, lo que permite al servidor analizarlos y procesarlos correctamente.
 
 
 
